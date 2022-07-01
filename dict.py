1ac8d21d3a76e32d78c45538a5e41fdadab16ade
Frank Martin <cedricmrtn@gmail.com>
2022-07-01 16:12:48 +0000

    A bit on error handling

--- dict.py
+++ dict.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
-cyborg = {'name': 'Victor Stone', 'first_appearance': 1980, 'birthPlace': 'Detroit', 'powers': ['super strength', 'self regeneration systems', 'advance sensory'], 'publisher': 'dc comics'}
+# cyborg = {'name': 'Victor Stone', 'first_appearance': 1980, 'birthPlace': 'Detroit', 'powers': ['super strength', 'self regeneration systems', 'advance sensory'], 'publisher': 'dc comics'}
 
-cyborg.update({"fav_food": "chimichangas"})
+# cyborg.update({"fav_food": "chimichangas"})
 
-print(cyborg.keys())
+# print(cyborg.keys())
 
-choice = input("Choose a key:")
-
-
+# choice = input("Choose a key:")
